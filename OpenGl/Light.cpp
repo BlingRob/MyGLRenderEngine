9d21af6189e51614145e27f110527bdd418d5a34
Стас Щетинин <gisaku2010@rambler.ru>
2021-02-02 11:30:58 +0300

    Light

--- OpenGl/Light.cpp
+++ OpenGl/Light.cpp
@@ -10,21 +10,51 @@ void Light::SendToShader(std::shared_ptr<Shader> shader)
 	glUniform1f(glGetUniformLocation(shader->Program, "light.quadratic"), quadratic);
 }
 
-void PointLight::SendToShader(std::shared_ptr<Shader> shader) 
+
+
+void AmbientLight::SendToShader(std::shared_ptr<Shader> shader) 
+{
+	Light::SendToShader(shader);
+	glUniform3f(glGetUniformLocation(shader->Program, "light.direction"), direction.x, direction.y, direction.z);
+}
+
+glm::vec3 AmbientLight::ChangeDirection(glm::vec3 NewDir) 
+{
+	std::swap(NewDir, direction);
+	return NewDir;
+}
+
+void PointLight::SendToShader(std::shared_ptr<Shader> shader)
 {
 	Light::SendToShader(shader);
 
 	glUniform3f(glGetUniformLocation(shader->Program, "light.position"), position.x, position.y, position.z);
 }
-
 glm::vec3 PointLight::SetPos(glm::vec3 p) 
 {
-	glm::vec3 OldPos = position;
-	position = p;
-	return OldPos;
+	std::swap(p, position);
+	return p;
 }
 
 glm::vec3 PointLight::GetPos() const
 {
 	return position;
+}
+
+void SpotLight::SendToShader(std::shared_ptr<Shader> shader) 
+{
+	AmbientLight::SendToShader(shader);
+	glUniform1f(glGetUniformLocation(shader->Program, "light.Theta"),Theta);
+	glUniform1f(glGetUniformLocation(shader->Program, "light.Alpha"), Alpha);
+}
+std::pair<float, float> SpotLight::GetAngels() 
+{
+	return std::make_pair(Theta,Alpha);
+}
+std::pair<float, float> SpotLight::GetAngels(std::pair<float, float> NewAngels) 
+{
+	std::pair<float, float> Old = std::make_pair(Theta, Alpha);
+	Theta = NewAngels.first;
+	Alpha = NewAngels.second;
+	return Old;
 }
--- OpenGl/Light.h
+++ OpenGl/Light.h
@@ -13,7 +13,7 @@ class Light
 	glm::vec3 specular;
 
 	public:
-		Light(float c = 0.0f, float l = 0.0f, float q = 0.0f, glm::vec3 a = glm::vec3(0.0f), glm::vec3 d = glm::vec3(0.0f), glm::vec3 s = glm::vec3(0.0f))
+	Light(float c = 0.0f, float l = 0.0f, float q = 0.0f, glm::vec3 a = glm::vec3(0.0f), glm::vec3 d = glm::vec3(0.0f), glm::vec3 s = glm::vec3(0.0f))
 		{
 			constant = c;
 			linear = l;
@@ -25,20 +25,43 @@ class Light
 		virtual void SendToShader(std::shared_ptr<Shader> shader);
 };
 
+class AmbientLight:public Light 
+{
+	glm::vec3 direction;
+
+public:
+	AmbientLight(float c = 0.0f, float l = 0.0f, float q = 0.0f, glm::vec3 a = glm::vec3(0.0f),
+		glm::vec3 d = glm::vec3(0.0f), glm::vec3 s = glm::vec3(0.0f), glm::vec3 p = glm::vec3(0.0f), glm::vec3 dir = glm::vec3(0.0f)) :
+		Light(c, l, q, a, d, s), direction(dir) {}
+	virtual void SendToShader(std::shared_ptr<Shader> shader);
+	glm::vec3 ChangeDirection(glm::vec3);
+};
+
 class PointLight : public Light
 {
 	glm::vec3 position;
 	public:
 	PointLight(float c = 0.0f, float l = 0.0f, float q = 0.0f, glm::vec3 a = glm::vec3(0.0f), glm::vec3 d = glm::vec3(0.0f), glm::vec3 s = glm::vec3(0.0f), glm::vec3 p = glm::vec3(0.0f)):
-		Light(c,l,q,a,d,s)
-	{
-		position = p;
-	}
+		Light(c,l,q,a,d,s),position(p){}
 	/*Return old pos, set new pos*/
-	glm::vec3 SetPos(glm::vec3 p);
+	virtual glm::vec3 SetPos(glm::vec3 p);
 	/*Return current pos*/
-	glm::vec3 GetPos() const;
-	void SendToShader(std::shared_ptr<Shader> shader);
+	virtual glm::vec3 GetPos() const;
+	virtual void SendToShader(std::shared_ptr<Shader> shader);
 
 };
 
+class SpotLight: public AmbientLight
+{
+	glm::vec3 direction;
+	float Theta; //angel of big cone
+	float Alpha; //angel of small cone
+	public:
+		SpotLight(float c = 0.0f, float l = 0.0f, float q = 0.0f, glm::vec3 a = glm::vec3(0.0f),
+			glm::vec3 d = glm::vec3(0.0f), glm::vec3 s = glm::vec3(0.0f), glm::vec3 p = glm::vec3(0.0f),glm::vec3 dir = glm::vec3(0.0f),
+			float BigAngel = 60, float SmallAngel = 30):AmbientLight(c, l, q, a, d, s, p, dir),Theta(BigAngel),Alpha(SmallAngel){}
+
+	void SendToShader(std::shared_ptr<Shader> shader);
+	std::pair<float, float> GetAngels();
+	std::pair<float, float> GetAngels(std::pair<float, float> NewAngels);
+};
